5cdfb19170428415312cadc6731c957e9fa0a4eb
Alexei Sadovin <61558947+BlackFox101@users.noreply.github.com>
2022-01-18 22:08:50 +0300

    initView

--- lw9/lw9/ChartDrawerView.cpp
+++ lw9/lw9/ChartDrawerView.cpp
@@ -1,12 +1,11 @@
-#include "ChartDrawer.h"
+#include "ChartDrawerView.h"
 #include <Windows.h>
 
 using namespace lw9;
-
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew ChartDrawer);
+	Application::Run(gcnew ChartDrawerView);
 	return 0;
 }
